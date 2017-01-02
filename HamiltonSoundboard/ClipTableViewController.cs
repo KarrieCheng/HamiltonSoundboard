d57421fd8f1da78cdbb3e31c9657997939b4bc75
Karrie Cheng <karriecheng1993@gmail.com>
2017-01-02 17:13:51 -0600

    Add clip TableView

---
+++ HamiltonSoundboard/ClipTableViewController.cs
@@ -0,0 +1,23 @@
+using Foundation;
+using System;
+using UIKit;
+using HamiltonSoundboard.Core;
+
+namespace HamiltonSoundboard
+{
+    public partial class ClipTableViewController: UITableViewController
+    {
+        ClipsDataService dataService = new ClipsDataService();
+        public ClipTableViewController (IntPtr handle) : base (handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            var clips = dataService.GetAllClips();
+            var dataSource = new ClipDataSource(clips, this);
+
+            TableView.Source = dataSource;
+        }
+    }
+}
---
+++ HamiltonSoundboard/DataSources/ClipDataSource.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UIKit;
+using System.Collections.Generic;
+using HamiltonSoundboard.Core;
+using Foundation;
+
+namespace HamiltonSoundboard
+{
+    public class ClipDataSource : UITableViewSource
+    {
+        private List<Clip> clips;
+        NSString cellIdentifier = new NSString("ClipCell");
+
+        public ClipDataSource(List<Clip> clips, UITableViewController callingController)
+        {
+            this.clips = clips;
+        }
+
+        public override nint RowsInSection(UITableView tableview, nint section)
+        {
+            return clips.Count;
+        }
+
+        public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+        {
+            UITableViewCell cell = tableView.DequeueReusableCell(cellIdentifier) as UITableViewCell;
+
+            if (cell == null)
+                cell = new UITableViewCell(UITableViewCellStyle.Default, cellIdentifier);
+
+            var clip = clips[indexPath.Row];
+            cell.TextLabel.Text = clip.Quote;
+
+            return cell;
+        }
+    }
+}
